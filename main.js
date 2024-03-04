5fd41d3a6c85eb42cf5f34ce98a705a6fb42797a
Krishna <125943937+Krishna-G-OP@users.noreply.github.com>
2024-03-04 11:27:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 async function generateRandomMeme() {
-    const response = await fetch('https://api.imgflip.com/get_memes');
-    const data = await response.json();
-    const memes = data.data.memes;
-    const randomIndex = Math.floor(Math.random() * memes.length);
-    const randomMeme = memes[randomIndex];
+            const response = await fetch('https://api.imgflip.com/get_memes');
+            const data = await response.json();
+            const memes = data.data.memes;
+            const randomIndex = Math.floor(Math.random() * memes.length);
+            const randomMeme = memes[randomIndex];
 
-    const memeURL = `https://api.imgflip.com/caption_image?template_id=${randomMeme.id}&username=divy_123&password=divynoone`;
-    // Replace 'your_username' and 'your_password' with your Imgflip API credentials
+            const memeURL = `https://api.imgflip.com/caption_image?template_id=${randomMeme.id}&username=your_username&password=your_password`;
+            // Replace 'your_username' and 'your_password' with your Imgflip API credentials
 
-    const response2 = await fetch(memeURL, { method: 'POST' });
-    const memeData = await response2.json();
+            const response2 = await fetch(memeURL, { method: 'POST' });
+            const memeData = await response2.json();
 
-    const memeImage = document.createElement("img");
-    memeImage.src = memeData.data.url;
+            const memeImage = document.createElement("img");
+            memeImage.src = memeData.data.url;
 
-    const memeContainer = document.getElementById("memeContainer");
-    memeContainer.innerHTML = "";
-    memeContainer.appendChild(memeImage);
+            const memeContainer = document.getElementById("memeContainer");
+            memeContainer.innerHTML = "";
+            memeContainer.appendChild(memeImage);
 }
-generateMemeBtn.addEventListener("click", generateRandomMeme);
-generateRandomMeme();
